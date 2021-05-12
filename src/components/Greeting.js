1254f8b4008f95143f3def0d3a95be1115d33594
Siddharth Rawat <siddharth.rawat94@gmail.com>
2021-05-12 21:23:31 +0530

    test user interaction & state

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
   return (
     <>
       <h2>Hello Human</h2>
-      <p>Greeting from outer space!</p>
+      {!changedText && <p>Greeting from outer space!</p>}
+      {/* <p>Greeting from outer space</p> */}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
     </>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,37 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Greeting from './Greeting';
 
 describe('Greeting Suite', () => {
-  test('Renders hello human as a text', () => {
-    //arrange
-    render(<Greeting />);
-    //act
-    //...nothing
+  test('Renders "hello human" as a text', () => {
+    render(<Greeting />); //arrange
+    const helloElement = screen.getByText(/hello human/i);
+    expect(helloElement).toBeInTheDocument(); //assert
+  });
+  test('Renders "greeting from outer space" when button was NOT clicked', () => {
+    render(<Greeting />); //arrange
+    const greetingElement = screen.getByText('Greeting from outer space', {
+      exact: false,
+    });
+    expect(greetingElement).toBeInTheDocument(); //assert
+  });
+  test('Render "changed" when button WAS clicked', () => {
+    render(<Greeting />); //arrange
+    //act -->  click the button!
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
     //assert
-    const helloElement = screen.getByText('Hello Human');
-    expect(helloElement).toBeInTheDocument();
+    const outputElement = screen.getByText('Changed', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+  test('Check if paragraph with "Greeting" text is not visible', () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    const hiddenTextElement = screen.queryByText('Greeting from outer space', {
+      exact: false,
+    });
+    expect(hiddenTextElement).toBeNull();
   });
 });
