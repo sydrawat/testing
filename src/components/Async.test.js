673ecaef173dd56176a7765294b1e8b06ffcf8cc
Siddharth Rawat <siddharth.rawat94@gmail.com>
2021-05-13 12:11:06 +0530

    using jest mock functions

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,8 +4,12 @@ import Async from './Async';
 describe('Async Component Test Suite', () => {
   test('Renders posts if requests succeeds', async () => {
     //arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First dummy post' }],
+    });
     render(<Async />);
-    //act
+    //act...nothing
     //assert
     const posts = await screen.findAllByRole('listitem');
     expect(posts).not.toHaveLength(0);
